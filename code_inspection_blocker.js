06b7efcb4388f7f0d48ac753c63184f210ac6254
Omkar Kamat <69665958+Omkar270048@users.noreply.github.com>
2024-11-26 10:39:15 +0530

    Create code_inspection_blocker.js
    Create code_inspection_blocker.js

---
+++ code_inspection_blocker.js
@@ -0,0 +1,17 @@
+// this code will block user to access website source code
+
+document.addEventListener('contextmenu', event=> event.preventDefault()); 
+    document.onkeydown = function(e) { 
+        if(event.keyCode == 123) { 
+        return false; 
+        } 
+        if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)){ 
+        return false; 
+        } 
+        if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)){ 
+        return false; 
+        } 
+        if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)){ 
+        return false; 
+        } 
+    }
